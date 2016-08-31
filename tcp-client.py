f38844c99e6b432b0f936a730458972026576061
agrantleung <agrantleung@126.com>
2016-08-31 11:42:20 +0800

    test PIL

--- tcp-client.py
+++ tcp-client.py
@@ -1,7 +1,7 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 建立连接:
-s.connect(('127.0.0.1', 9998))
+s.connect(('192.168.63.128', 9998))
 # 接收欢迎消息:
 print(s.recv(1024).decode('utf-8'))
 for data in [b'Michael', b'Tracy', b'Sarah']:
