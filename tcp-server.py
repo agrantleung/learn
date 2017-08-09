5523f76e141d7b0bbd3817a88b1ac53034818838
agrantleung <agrantleung@126.com>
2017-08-09 18:03:52 +0800

    test

--- tcp-server.py
+++ tcp-server.py
@@ -1,14 +1,11 @@
 import socket
+import threading
+import time
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('127.0.0.1', 9998))
 s.listen(5)
 print('Waiting for connection...')
-while True:
-    # 接受一个新连接:
-    sock, addr = s.accept()
-    # 创建新线程来处理TCP连接:
-    t = threading.Thread(target=tcplink, args=(sock, addr))
-    t.start()
+
 
 def tcplink(sock, addr):
     print('Accept new connection from %s:%s...' % addr)
@@ -21,3 +18,10 @@ def tcplink(sock, addr):
         sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
     sock.close()
     print('Connection from %s:%s closed.' % addr)
+
+while True:
+    # 接受一个新连接:
+    sock, addr = s.accept()
+    # 创建新线程来处理TCP连接:
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
